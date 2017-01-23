c2979bd0cfb890a544800021a36d0dd097cc9679
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-01-23 10:40:17 -0800

    C#

---
+++ Beginner/1099/1099/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1099
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b;
+            int N = int.Parse(Console.ReadLine());
+            int i = 1;
+            for (i = 1; i <= 7; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                a = int.Parse(tokens[0]);
+                b = int.Parse(tokens[1]);
+
+                while (a <= b)
+                {
+
+                }
+            }
+        }
+    }
+}
